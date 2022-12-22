64c3094f6c00cc7b716c6985cde0dccdc62a4dc3
shobeee <108212593+shobeee@users.noreply.github.com>
2022-12-22 10:13:43 +0500

    completed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
+  mode:"development",
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html")
       })
-    ]
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
   };
